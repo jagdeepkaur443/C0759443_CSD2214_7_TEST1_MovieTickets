14e2be7e1741b3045c08ff549992b4d19bf80edc
jagdeepkaur443 <jass28081992@gmail.com>
2021-03-11 21:40:50 -0500

    folder restructure and validation

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 function selectmovie(){
     var e = document.getElementById("movies");
-    text = e.options[e.selectedIndex].text;
+    var text = e.options[e.selectedIndex].text;
     document.getElementById("movieSelected").innerHTML = text;
     if(text == "Iron Man ($10)"){
         document.getElementById("ticketPrice").innerHTML = "$10";
@@ -18,4 +18,9 @@ function selectmovie(){
 function seatSelect(){
     x = document.getElementsByClassName("seat");
     x[0].style.backgroundColor = 'red';
+}
+
+function totalPrice(){
+    var tickets = document.getElementById("tickets").value;
+    if()
 }
