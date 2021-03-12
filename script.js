2071a164812e40b968d8a60d29128e6431f66e75
jagdeepkaur443 <jass28081992@gmail.com>
2021-03-11 20:25:43 -0500

    showing selected movie deatils

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function selectmovie(){
-var e = document.getElementById("movies");
-var text = e.options[e.selectedIndex].text;
-console.log(text);
+    var e = document.getElementById("movies");
+    var text = e.options[e.selectedIndex].text;
+    document.getElementById("movieSelected").innerHTML = text;
+    if(text == "Iron Man ($10)"){
+        document.getElementById("ticketPrice").innerHTML = "$10";
+    }else if(text == "Avengers ($25)"){
+        document.getElementById("ticketPrice").innerHTML = "$20";
+    }else if(text == "Jab We Met ($10)"){
+        document.getElementById("ticketPrice").innerHTML = "$10";
+    }else if(text == "Dil Hai Tuhara ($12)"){
+        document.getElementById("ticketPrice").innerHTML = "$12";
+    }else{
+        document.getElementById("ticketPrice").innerHTML = "";
+    }
+}
+
+function seatSelect(){
+    x = document.getElementsByClassName("seat");
+    x[0].style.backgroundColor = 'red';
 }
